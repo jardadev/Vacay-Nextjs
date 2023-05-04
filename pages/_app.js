be2a20c6e8a192118bb4d3b8105424e77cd674b5
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-03 17:55:47 -0700

    add raleway from next google fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Raleway } from 'next/font/google';
+
+// Google font import
+const raleway = Raleway({
+	subsets: ['latin'],
+	variable: '--font-raleway',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<main className={`${raleway.variable}`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.jsx'],
+	theme: {
+		extend: {
+			fontFamily: {
+				raleway: ['var(--font-raleway)'],
+			},
+		},
+	},
 	plugins: [],
 };
