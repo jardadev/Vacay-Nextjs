f1cb972ca5be1f123fa07c4ab4d2228afd5dfe5c
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-03 17:54:14 -0700

    add nav, add heroicons

--- components/UI/Nav.jsx
+++ components/UI/Nav.jsx
@@ -1,58 +1,26 @@
-import { Navbar, Dropdown, Avatar } from 'flowbite-react';
-import Image from 'next/image';
-import React from 'react';
+import Link from 'next/link';
+import {
+	HomeModernIcon,
+	UserIcon,
+	ChevronDownIcon,
+} from '@heroicons/react/24/outline';
 
 const Nav = () => {
 	return (
-		<Navbar fluid={true} rounded={true}>
-			<Navbar.Brand href='https://flowbite.com/'>
-				<Image
-					src='https://flowbite.com/docs/images/logo.svg'
-					className='mr-3 h-6 sm:h-9'
-					alt='Flowbite Logo'
-          width={30}
-          height={30}
-				/>
-				<span className='self-center whitespace-nowrap text-xl font-semibold dark:text-white'>
-					Flowbite
-				</span>
-			</Navbar.Brand>
-			<div className='flex md:order-2'>
-				<Dropdown
-					arrowIcon={false}
-					inline={true}
-					label={
-						<Avatar
-							alt='User settings'
-							img='https://flowbite.com/docs/images/people/profile-picture-5.jpg'
-							rounded={true}
-						/>
-					}
-				>
-					<Dropdown.Header>
-						<span className='block text-sm'>Bonnie Green</span>
-						<span className='block truncate text-sm font-medium'>
-							name@flowbite.com
-						</span>
-					</Dropdown.Header>
-					<Dropdown.Item>Dashboard</Dropdown.Item>
-					<Dropdown.Item>Settings</Dropdown.Item>
-					<Dropdown.Item>Earnings</Dropdown.Item>
-					<Dropdown.Divider />
-					<Dropdown.Item>Sign out</Dropdown.Item>
-				</Dropdown>
-				<Navbar.Toggle />
+		<nav className='h-16 w-full shadow-md'>
+			<div className='h-full container mx-auto px-4 flex justify-between items-center space-x-4'>
+				<div className='flex space-x-1 items-center'>
+					<HomeModernIcon className='shrink-0 w6 h-6 text-rose-500' />
+					<Link href={'/'} className='text-lg font-bold'>
+						VacayNextJs
+					</Link>
+				</div>
+				<div className='flex items-center justify-between'>
+					<UserIcon className='shrink-0 w-8 h-8 text-grey-500' />
+					<ChevronDownIcon className='w-4 h-4 text-black' />
+				</div>
 			</div>
-			<Navbar.Collapse>
-				<Navbar.Link href='/navbars' active={true}>
-					Home
-				</Navbar.Link>
-				<Navbar.Link href='/navbars'>About</Navbar.Link>
-				<Navbar.Link href='/navbars'>Services</Navbar.Link>
-				<Navbar.Link href='/navbars'>Pricing</Navbar.Link>
-				<Navbar.Link href='/navbars'>Contact</Navbar.Link>
-			</Navbar.Collapse>
-		</Navbar>
+		</nav>
 	);
 };
 
