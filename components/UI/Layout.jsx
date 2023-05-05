e24f9d557140266c84a4da3c108e09eb3f7c0911
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-05 14:42:23 -0700

    replace flex styling

--- components/UI/Layout.jsx
+++ components/UI/Layout.jsx
@@ -4,8 +4,8 @@ const Layout = ({ children }) => {
 	return (
 		<div className='min-h-screen flex flex-col font-raleway'>
 			<Nav />
-			<div className='flex-grow container'>
-				<main className='py-12 lg:px-4'>{children}</main>
+			<div className='px-4 py-12 lg:container lg:mx-auto'>
+				<main className=''>{children}</main>
 			</div>
 		</div>
 	);
