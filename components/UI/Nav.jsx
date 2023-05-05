765bc5ed8a4ed5aa1dbaa9bf5ff61dc10199ec95
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-05 14:40:27 -0700

    add nav logic and styling

--- components/UI/Nav.jsx
+++ components/UI/Nav.jsx
@@ -3,21 +3,105 @@ import {
 	HomeModernIcon,
 	UserIcon,
 	ChevronDownIcon,
+	PlusIcon,
+	HomeIcon,
+	HeartIcon,
+	LogoutIcon,
 } from '@heroicons/react/24/outline';
+import ThemeToggle from './ThemeToggle';
+import { themeChange } from 'theme-change';
 
 const Nav = () => {
+	const menuItems = [
+		{
+			label: 'List a new home',
+			icon: PlusIcon,
+			href: '/homes/create',
+		},
+		{
+			label: 'My homes',
+			icon: HomeIcon,
+			href: '/homes',
+		},
+		{
+			label: 'Favorites',
+			icon: HeartIcon,
+			href: '/favorites',
+		},
+		{
+			label: 'Logout',
+			icon: LogoutIcon,
+			onClick: null,
+		},
+	];
+
+	/*     display: flex;
+    align-items: center;
+    padding: var(--navbar-padding, 0.5rem);
+    min-height: 4rem;
+    width: 100%; */
 	return (
-		<nav className='h-16 w-full shadow-md'>
-			<div className='h-full container mx-auto px-4 flex justify-between items-center space-x-4'>
-				<div className='flex space-x-1 items-center'>
-					<HomeModernIcon className='shrink-0 w6 h-6 text-rose-500' />
-					<Link href={'/'} className='text-lg font-bold'>
-						VacayNextJs
+		<nav className='shadow-md'>
+			<div className='flex items-center min-h-16 px-4 justify-between lg:container lg:mx-auto lg:text-lg'>
+				<div>
+					<Link href={'/'} className='flex items-center gap-1'>
+						<HomeModernIcon className='h-8 text-secondary' />
+						<div>
+							Vacay
+							<span className='text-secondary'>Nextjs</span>
+						</div>
 					</Link>
 				</div>
-				<div className='flex items-center justify-between'>
-					<UserIcon className='shrink-0 w-8 h-8 text-grey-500' />
-					<ChevronDownIcon className='w-4 h-4 text-black' />
+				<div className='flex items-center'>
+					{/* Home CRUD UI Dropdown Button */}
+					<div className='dropdown dropdown-end'>
+						<label
+							tabIndex={0}
+							className='btn btn-xs btn-ghost btn-circle'
+						>
+							<ChevronDownIcon />
+						</label>
+						<ul
+							tabIndex={0}
+							className='menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52'
+						>
+							{menuItems.map(({ label, href, icon: Icon }) => (
+								<li key={label}>
+									{href && (
+										<Link href={href}>
+											<div className='flex items-center space-x-2 py-2 px-4'>
+												<Icon className='w-5 h-5 shrink-0 text-gray-500' />
+												<span>{label}</span>
+											</div>
+										</Link>
+									)}
+								</li>
+							))}
+						</ul>
+					</div>
+
+					<div className='dropdown dropdown-end'>
+						<label
+							tabIndex={0}
+							className='btn btn-ghost btn-circle avatar'
+						>
+							<div className='w-10 rounded-full bg-white'></div>{' '}
+							{/* FIXME: Replace bg style with user image from session. */}
+						</label>
+						<div
+							tabIndex={0}
+							className='flex items-center mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow'
+						>
+							<div className='card-body'>
+								<ThemeToggle />
+								<div className='card-actions items-center text-center'>
+									<button className='btn btn-primary btn-outline w-full'>
+										Logout
+									</button>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</nav>
