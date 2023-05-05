2c30b30f3e649c838ac5db9c1ed5a4fdcbf5ddfe
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-05 13:34:50 -0700

    add styles for padding in layout

--- components/UI/Layout.jsx
+++ components/UI/Layout.jsx
@@ -4,7 +4,9 @@ const Layout = ({ children }) => {
 	return (
 		<div className='min-h-screen flex flex-col font-raleway'>
 			<Nav />
-			<main className='lg:container lg:mx-auto'>{children}</main>
+			<div className='flex-grow container'>
+				<main className='py-12 lg:px-4'>{children}</main>
+			</div>
 		</div>
 	);
 };
