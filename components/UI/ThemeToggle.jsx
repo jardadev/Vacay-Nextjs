2c7ca8d3dd6e593e64839c193e9ccee2806b8855
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-05 14:43:27 -0700

    add theme toggle component

---
+++ components/UI/ThemeToggle.jsx
@@ -0,0 +1,54 @@
+'use client';
+import { useEffect } from 'react';
+import { SunIcon, MoonIcon } from '@heroicons/react/24/outline';
+
+export default function ThemeToggle({}) {
+	useEffect(() => {
+		[...document.querySelectorAll('[data-toggle-theme]')].forEach((el) => {
+			el.addEventListener('click', toggleTheme);
+		});
+
+		return () =>
+			[...document.querySelectorAll('[data-toggle-theme]')].forEach(
+				(el) => el.removeEventListener('click', toggleTheme)
+			);
+	}, []);
+
+	return (
+		<div className='flex gap-2 h-6'>
+			<MoonIcon className='' />
+			<input
+				type='checkbox'
+				className='toggle'
+				data-toggle-theme='light,dark'
+
+			/>
+			<SunIcon />
+		</div>
+	);
+}
+
+function toggleTheme(evt) {
+	var themesList = evt.target.getAttribute('data-toggle-theme');
+	if (themesList) {
+		var themesArray = themesList.split(',');
+		if (
+			document.documentElement.getAttribute('data-theme') ==
+			themesArray[0]
+		) {
+			if (themesArray.length == 1) {
+				document.documentElement.removeAttribute('data-theme');
+				localStorage.removeItem('theme');
+			} else {
+				document.documentElement.setAttribute(
+					'data-theme',
+					themesArray[1]
+				);
+				localStorage.setItem('theme', themesArray[1]);
+			}
+		} else {
+			document.documentElement.setAttribute('data-theme', themesArray[0]);
+			localStorage.setItem('theme', themesArray[0]);
+		}
+	}
+}
