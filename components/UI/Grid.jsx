17e6e5337c377b5227dd555ffba518e78fc7ba09
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-06 11:07:51 -0700

    add home display grid component

---
+++ components/UI/Grid.jsx
@@ -0,0 +1,123 @@
+import { ExclamationCircleIcon } from '@heroicons/react/24/outline';
+import Card from './Card';
+
+const Grid = ({
+	homes = [
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/nav.jpeg',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+		{
+			id: 0,
+			image: '/images/100devs.png',
+			title: 'Hello World',
+			guests: 1,
+			beds: 2,
+			baths: 3,
+			price: 400,
+			favorite: false,
+		},
+	],
+}) => {
+	const isEmpty = homes.length === 0;
+
+	return isEmpty ? (
+		// FIXME: Replace warning styles
+		<p className='text-error-content bg-warning px-4 rounded-md py-2 max-w-max inline-flex items-center space-x-1'>
+			<ExclamationCircleIcon className='shrink-0 w-5 h-5 mt-px' />
+			<span className='text-xs lg:text-sm'>
+				Unfortunately, there is nothing to display yet.
+			</span>
+		</p>
+	) : (
+		<div className='grid justify-center items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6'>
+			{homes.map((home) => (
+				<Card
+					key={home.id}
+					{...home}
+					// onClickFavorite={toggleFavorite}
+					// favorite={!!favorites.includes(home.id)}
+					favorite={true}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default Grid;
