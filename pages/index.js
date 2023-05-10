f0151c3f7e8b76d5fe34a3cae9920fe54fd2b558
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-10 10:46:27 -0700

    add grid and content

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,26 @@
 import Layout from '@/components/UI/Layout';
+import Grid from '@/components/UI/Grid';
 
 const Home = () => {
-	return <Layout>VacayNextjs</Layout>;
+	return (
+		<Layout>
+			<div>
+				<section>
+					<header className='leading-8'>
+						<h2 className='font-bold text-primary'>
+							Top rated places to stay:
+						</h2>
+						<p className='text-gray-500 text-xs lg:text-sm'>
+							Explore some of the best homes in the world!
+						</p>
+					</header>
+				</section>
+				<section className='py-6'>
+					<Grid />
+				</section>
+			</div>
+		</Layout>
+	);
 };
 
 export default Home;
