facc372de374f9508c9ae8ec37e293db7ffcd165
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-11 14:15:30 -0700

    fix Icon imports

--- components/UI/AuthModal.jsx
+++ components/UI/AuthModal.jsx
@@ -7,7 +7,11 @@ import * as Yup from 'yup';
 import { toast } from 'react-hot-toast';
 import { Formik, Form } from 'formik';
 import { Dialog, Transition } from '@headlessui/react';
-import { SparklesIcon, MailOpenIcon, XIcon } from '@heroicons/react/24/outline';
+import {
+	SparklesIcon,
+	EnvelopeOpenIcon,
+	XMarkIcon,
+} from '@heroicons/react/24/outline';
 import Input from '../Input';
 
 const SignInSchema = Yup.object().shape({
@@ -45,7 +49,7 @@ const Confirm = ({ show = false, email = '' }) => (
 					<div className='overflow-hidden transition-all transform'>
 						<h3 className='text-center text-lg font-medium leading-6'>
 							<div className='flex flex-col justify-center items-center space-y-4'>
-								<MailOpenIcon className='w-12 h-12 shrink-0 text-rose-500' />
+								<EnvelopeOpenIcon className='w-12 h-12 shrink-0 text-rose-500' />
 							</div>
 							<p className='text-2xl font-semibold mt-2'>
 								Confirm your email
@@ -172,22 +176,23 @@ const AuthModal = ({ show = false, onClose = () => null }) => {
 								onClick={closeModal}
 								className='absolute top-2 right-2 shrink-0 p-1 rounded-md hover:bg-gray-100 transition focus:outline-none'
 							>
-								<XIcon className='w-5 h-5' />
+								<XMarkIcon className='w-5 h-5' />
 							</button>
 
 							<div className='py-12'>
 								<div className='px-4 sm:px-12'>
 									<div className='flex justify-center'>
-										<Link href='/'>
-											<a className='flex items-center space-x-1'>
-												<SparklesIcon className='shrink-0 w-8 h-8 text-rose-500' />
-												<span className='text-xl font-semibold tracking-wide'>
-													Vacay
-													<span className='text-rose-500'>
-														Nextjs
-													</span>
+										<Link
+											href='/'
+											className='flex items-center space-x-1'
+										>
+											<SparklesIcon className='shrink-0 w-8 h-8 text-rose-500' />
+											<span className='text-xl font-semibold tracking-wide'>
+												Vacay
+												<span className='text-rose-500'>
+													Nextjs
 												</span>
-											</a>
+											</span>
 										</Link>
 									</div>
 
@@ -216,7 +221,7 @@ const AuthModal = ({ show = false, onClose = () => null }) => {
 											className='h-[46px] w-full mx-auto border rounded-md p-2 flex justify-center items-center space-x-2 text-gray-500 hover:text-gray-600 hover:border-gray-400 hover:bg-gray-50 focus:outline-none focus:ring-4 focus:ring-gray-400 focus:ring-opacity-25 disabled:opacity-50 disabled:cursor-not-allowed disabled:hover:text-gray-500 disabled:hover:bg-transparent disabled:hover:border-gray-200 transition-colors'
 										>
 											<Image
-												src='/google.svg'
+												src='images/google.svg'
 												alt='Google'
 												width={32}
 												height={32}
