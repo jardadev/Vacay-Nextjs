73ec31b493331e07f7439e6f0bf57f260c015fc2
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-11 14:15:56 -0700

    add auth modal to app layout

--- components/UI/Layout.jsx
+++ components/UI/Layout.jsx
@@ -1,11 +1,22 @@
 import Nav from './Nav';
+import AuthModal from './AuthModal';
+import { useState } from 'react';
 
 const Layout = ({ children }) => {
+	const [showModal, setShowModal] = useState(false);
+	const openModal = () => setShowModal(true);
+	const closeModal = () => setShowModal(false);
+
 	return (
 		<div className='min-h-screen flex flex-col'>
-			<Nav />
+			<Nav openModal={openModal} />
 			<div className='px-4 py-24 lg:container lg:mx-auto'>
-				<main>{children}</main>
+				<main>
+					{typeof children === 'function'
+						? children(openModal)
+						: children}
+				</main>
+				<AuthModal show={showModal} onClose={closeModal} />
 			</div>
 		</div>
 	);
