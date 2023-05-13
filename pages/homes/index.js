da0f04726316e2b71df62158234b98162a30df72
AARON JOHNSON <jardaaroncs@gmail.com>
2023-05-12 17:09:21 -0700

    add session protection

--- pages/homes/index.js
+++ pages/homes/index.js
@@ -1,12 +1,13 @@
 import { prisma } from '@/lib/db';
+import { getSession } from 'next-auth/react';
 import Layout from '@/components/UI/Layout';
 import Grid from '@/components/UI/Grid';
 
 const Home = ({ homes = [] }) => {
 	return (
 		<Layout>
-			<h1 className='text-xl font-medium text-gray-800'>Your listings</h1>
-			<p className='text-gray-500'>
+			<h1 className='text-xl font-medium text-primary'>Your listings</h1>
+			<p className='text-secondary'>
 				Manage your homes and update your listings
 			</p>
 			<div className='mt-8'>
@@ -17,3 +18,30 @@ const Home = ({ homes = [] }) => {
 };
 
 export default Home;
+
+export async function getServerSideProps(context) {
+	// Check if user is authenticated
+	const session = await getSession(context);
+
+	// If not, redirect to the homepage
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
+	// Get all homes of authenticated user
+	const homes = await prisma.home.findMany({
+		where: { owner: { email: session.user.email } },
+		orderBy: { createdAt: 'desc' },
+	});
+
+	return {
+		props: {
+			homes: JSON.parse(JSON.stringify(homes)),
+		},
+	};
+}
